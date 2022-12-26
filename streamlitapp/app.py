5f94bb659f5c6ee3fe574299f629edb3fabe2eb3
nooshinmn <71963260+nooshinmn@users.noreply.github.com>
2022-12-25 22:20:18 -0800

    Update app.py

--- streamlitapp/app.py
+++ streamlitapp/app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import streamlit as st
-from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from PIL import Image
